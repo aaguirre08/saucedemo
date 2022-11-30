7809ea177bf15c35aaa3200d5fb32242da7955e7
Angélica Aguirre <angelicaaguirre@Angelicas-MacBook-Pro.local>
2022-11-30 12:54:58 -0600

    Scenarios for session closing

---
+++ cypress/e2e/logOut/successful-logout.cy.js
@@ -0,0 +1,37 @@
+/// <reference types="cypress" />
+
+import login from '../../pages/loginPage';
+import inventory from '../../pages/inventoryPage';
+
+describe('Successful logout', () => {
+
+    before(() => {
+        cy.visit('/')
+    });
+
+    const userInfo = [
+        {
+            scenario: 'Performance User',
+            user: 'performance_glitch_user'
+        },
+        {
+            scenario: 'Problem User',
+            user: 'problem_user'
+        },
+        {
+            scenario: 'Standard User',
+            user: 'standard_user'
+        }
+    ]
+
+    userInfo.forEach(testCase => {
+        it(testCase.scenario, () => {
+            login.typeUserName(testCase.user)
+            login.typePass('secret_sauce')
+            login.clickLogin()
+            inventory.clickMenu()
+            inventory.clickLogOut()
+            cy.url().should('eq', Cypress.config().baseUrl + '/')
+        });
+    });
+});
