3766ab0d77d3e44ca23959d8456f29ea7a5ad8ad
Angélica Aguirre <angelicaaguirre@Angelicas-MacBook-Pro.local>
2022-11-30 18:50:10 -0600

    change configuration

--- cypress.config.js
+++ cypress.config.js
@@ -3,12 +3,11 @@ const { defineConfig } = require("cypress");
 module.exports = defineConfig({
   projectId: "w3q718",
   chromeWebSecurity: false,
-  retries: {
-    runMode: 2,
-  },
   watchForFileChanges: false,
   e2e: {
     baseUrl: 'https://www.saucedemo.com',
     specPattern: 'cypress/e2e/**/*.{js,jsx,ts,tsx}',
   },
+  videoCompression: false,
+  videoUploadOnPasses: false
 });
